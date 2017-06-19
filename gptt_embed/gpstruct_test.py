8e75c733bde6497d0956eba24a925a99b80d579b
Pavel Izmailov <izmailovpavel@gmail.com>
2017-06-20 00:48:59 +0300

    Initialized tests for TTGPstruct

---
+++ gptt_embed/gpstruct_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+from gpstruct import TTGPstruct
+
+class TTGPstructTest(tf.test.TestCase):
+  """Tests for TTGPstruct class methods.
+  """
+
+  def test_pairwise_dists(self):
+    """Tests `_compute_pairwise_dists` method.
+    """
+    pass
+
+  def test_latent_vars_distribution(self):
+    """Tests `_latent_vars_distribution` method.
+    """
+    pass
+
