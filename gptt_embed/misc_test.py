a7b5ba6200a7103113c0edf7ee92858063ba67cd
Pavel Izmailov <izmailovpavel@gmail.com>
2017-06-20 00:57:27 +0300

    Initialized tests for misc module

---
+++ gptt_embed/misc_test.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+import tensorflow as tf
+
+from gpstruct import TTGPstruct
+
+class MiscTest(tf.test.TestCase):
+  """Tests functions from `misc` module.
+  """
+
+  def test_kron_sequence_pairwise_quadratic_form(self):
+    """Tests `_kron_sequence_pairwise_quadratic_form` function.
+    """
+    pass
+
+  def test_pairwise_quadratic_form(self):
+    """Tests `pairwise_quadratic_form` function.
+    """
+    pass
+
+  def test_kron_logdet(self):
+    """Tests `_kron_logdet` function.
+    """
+    pass
+
+  def test_kron_tril(self):
+    """Tests `_kron_tril` function.
+    """
+    pass
+
+  def test_mse_r2(self):
+    """Tests `mse` and `r2` functions.
+    """
+    pass
+
+  def test_accuracy_num_correct(self):
+    """Tests `accuracy` and `num_correct` function.
+    """
+    pass
